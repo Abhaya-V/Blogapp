6b3b541a13bebab726f135a0e41a96431edade6e
Abhaya-V <abhayaviswakumar@gmail.com>
2025-04-07 12:31:54 +0530

    added vercel

--- Frontend/src/axiosInterceptor.js
+++ Frontend/src/axiosInterceptor.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:4000"
+    baseURL:"https://blogapp-server-six.vercel.app/"
 })
 
 axiosInstance.interceptors.request.use(function (config) {
